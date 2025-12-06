fc17e06190b42f4b2314e59faf1c4da815094ac1
Hardik Mittal <hardikmittal12@gmail.com>
2025-12-07 03:46:39 +0530

    Feature: Add User model, registration controller, routes, and JWT authentication

---
+++ backend/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minlength: 8,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+//   no validation in password field for complexity because it is handled in the controller and logic for password strength may be updated in future
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model("User", userSchema);
